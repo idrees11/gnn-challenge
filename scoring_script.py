a1dbd660fb13a621669ef3c9ccf79bd99456282c
Mohammad Idrees Bhat <29601026+idrees11@users.noreply.github.com>
2026-01-01 09:59:33 +0530

    Update scoring_script.py

--- scoring_script.py
+++ scoring_script.py
@@ -1,1 +1,10 @@
+import sys
+import pandas as pd
+from sklearn.metrics import f1_score
+
+submission = pd.read_csv(sys.argv[1])
+truth = pd.read_csv("data/test_labels.csv")
+
+score = f1_score(truth.target, submission.target, average="macro")
+print(f"Submission F1 Score: {score:.4f}")
 
