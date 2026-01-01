1908c3fa5b0665a85bd5835b557d5db6aaba94d6
Mohammad Idrees Bhat <29601026+idrees11@users.noreply.github.com>
2026-01-01 09:58:51 +0530

    Update model.py

--- starter_code/model.py
+++ starter_code/model.py
@@ -1,1 +1,40 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GINConv, global_mean_pool
+
+class GINModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim=32, output_dim=2):
+        super().__init__()
+
+        self.conv1 = GINConv(
+            nn.Sequential(
+                nn.Linear(input_dim, hidden_dim),
+                nn.ReLU(),
+                nn.Linear(hidden_dim, hidden_dim)
+            )
+        )
+
+        self.conv2 = GINConv(
+            nn.Sequential(
+                nn.Linear(hidden_dim, hidden_dim),
+                nn.ReLU(),
+                nn.Linear(hidden_dim, hidden_dim)
+            )
+        )
+
+        self.lin = nn.Sequential(
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(hidden_dim, output_dim)
+        )
+
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+        x = self.conv1(x, edge_index)
+        x = self.conv2(x, edge_index)
+        x = global_mean_pool(x, batch)
+        x = self.lin(x)
+        return F.log_softmax(x, dim=1)
 
